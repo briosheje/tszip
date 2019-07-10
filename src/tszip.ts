69782066d979c1c9fecbe0221223e99c605ed708
briosheje <uaesbriosheje@gmail.com>
2019-07-10 06:55:45 +0200

    Added examples to the docs.

--- src/tszip.ts
+++ src/tszip.ts
@@ -4,6 +4,23 @@
 /**
  * Make an iterator that aggregates elements from each of the iterables.
  *
+ * @example
+ * ```
+ * // Sample usage
+ * let [a,b] = [[1,2],[3,4]];
+ * let zipped = [...zip(a,b)]; // or Array.from(zip(a,b));
+ * console.log(zipped); // [[1,3],[2,4]]
+ * ```
+ * @example
+ * ```
+ * // Usage as iterable.
+ * let [a,b] = [[1,2],[3,4]];
+ * for (var [c,d] of zip(a,b)) {
+ *  // c will be 1, then 3
+ *  // d will be 2, then 4.
+ *  console.log('c id', c, 'd is ', d);
+ * ```
+ * }
  * @export
  * @param {...Array<any>[]} args
  * @returns {IterableIterator<any[]>}
